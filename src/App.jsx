853687897359110aa9198d9cf2ecbe9d16b90d29
Rohit <singhrohit51967@gmail.com>
2023-10-28 15:37:21 +0530

    Adding localStorage Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import TodoForm from './Components/TodoForm'
 import TodoItem from './Components/TodoItem'
@@ -23,6 +23,20 @@ function App() {
     setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? { ...prevTodo, completed: !prevTodo.completed } : prevTodo)));
   }
 
+  // Adding local storage
+
+  useEffect(() => {
+    const todos = (JSON.parse(localStorage.getItem('todos')));
+
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
   return (
     <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
       <div className="bg-[#172842] min-h-screen py-8">
@@ -32,7 +46,9 @@ function App() {
             <TodoForm />
           </div>
           <div className="flex flex-wrap gap-y-3">
-            <TodoItem />
+            {todos.map((todo) => (<div key={todo.id} className='w-full'>
+              <TodoItem todo={todo} />
+            </div>))}
           </div>
         </div>
       </div>
